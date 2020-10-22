9ed1d0a983d1a0a46a4e669e4098a89ad51211f1
Suyash Singh <suyashsingh234@gmail.com>
2020-10-23 02:05:15 +0530

    prediction and loss function mse

--- gru/gru.py
+++ gru/gru.py
@@ -16,8 +16,8 @@ from data import create_data
 x_train=np.array(x_train)
 y_train=np.array(y_train)
 
-print(x_train.shape)
-print(y_train.shape)
+x_train=x_train.astype("float32")
+y_train=y_train.astype("float32")
 
 model=keras.Sequential()
 model.add(keras.Input(shape=(500,5)))
@@ -25,12 +25,13 @@ model.add(keras.layers.GRU(5,activation='relu'))
 model.add(keras.layers.Dense(1*5))
 
 model.compile(
-        loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+        loss=keras.losses.MeanSquaredError(),
         optimizer=keras.optimizers.Adam(lr=0.001),
         metrics=["accuracy"]
     )
 
 model.fit(x_train,y_train)
 
+test=np.array([x_train[1]])
 
-#model.evaluate(x_test,y_test)
+print( model.predict(test) )
