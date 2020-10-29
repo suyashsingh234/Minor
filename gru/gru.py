79f75e025bce0d053006a5a233e6c7928a722d40
Suyash Singh <suyashsingh234@gmail.com>
2020-10-29 17:02:41 +0530

    changed loss function, song generation attempt

--- gru/gru.py
+++ gru/gru.py
@@ -21,14 +21,17 @@ y_train=np.array(y_train)
 x_train=x_train.astype("int")
 y_train=y_train.astype("int")
 
+x_train=x_train[:500]
+y_train=y_train[:500]
+
 model=keras.Sequential()
 model.add(keras.Input(shape=(500,5)))
-model.add(keras.layers.GRU(250,activation='relu'))
+model.add(keras.layers.GRU(5,activation='linear'))
 model.add(keras.layers.Dense(1*5))
 
 model.compile(
-        loss=keras.losses.MeanSquaredError(),
-        optimizer=keras.optimizers.Adam(lr=0.1),
+        loss=keras.losses.MeanAbsoluteError(),
+        optimizer=keras.optimizers.Adam(lr=0.001),
         metrics=["accuracy"]
     )
 
@@ -36,50 +39,35 @@ model.fit(x_train,y_train)
 
 # song generation
 
-def first_note():
-    a=random.randint(0, 1)
-    b=0
-    c=random.randint(0,100)
-    d=random.randint(0,127)
-    e=random.randint(0, 200)
-    return [a,b,c,d,e]
-
-def fillZeros(list,i):
-    j=i+1;
-    while j<500:
-        list.append([0,0,0,0,0])
-        j+=1
-    return list
-    
-
 mid = MidiFile()
 track = MidiTrack()
 
-currentNote=[]    
-currentNote.append(first_note())
-currentNote=fillZeros(currentNote,0)
+currentNote=x_train[0].tolist()
 
 i=0
 while i<499:
     feed=[currentNote]
     feed=np.array(feed)
     output=model.predict(feed)
-    output=np.absolute(np.rint(output))
+    output=np.absolute(output)
     output=output[0].astype("int").tolist()
+    print(output)
     currentNote[i+1]=output
     i+=1 
 
-for note in currentNote:
-    print(note)
-    (a,b,c,d,e)=note
-    if a:
-        a='note_on'
-    else:
-        a='note_off'
-    track.append(Message(a,channel=b,note=c,velocity=d,time=e))
-
-mid.tracks.append(track)        
-mid.save('song_gru.mid')
+# =============================================================================
+# for note in currentNote:
+#     print(note)
+#     (a,b,c,d,e)=note
+#     if a:
+#         a='note_on'
+#     else:
+#         a='note_off'
+#     track.append(Message(a,channel=b,note=c,velocity=d,time=e))
+# 
+# mid.tracks.append(track)        
+# mid.save('song_gru.mid')
+# =============================================================================
 # =============================================================================
 # test=np.array([x_train[1]])
 # 
