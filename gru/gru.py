dfd6374af6df1ffa984796a1d69fd4cfa550a953
Suyash Singh <suyashsingh234@gmail.com>
2020-10-31 22:18:36 +0530

    normalization

--- gru/gru.py
+++ gru/gru.py
@@ -18,11 +18,25 @@ from data import create_data
 x_train=np.array(x_train)
 y_train=np.array(y_train)
 
-x_train=x_train.astype("int")
-y_train=y_train.astype("int")
+x_train=x_train.astype("float32")
+y_train=y_train.astype("float32")
 
 x_train=x_train[:500]
 y_train=y_train[:500]
+
+def normalize(list):
+    for i in range(0,len(list)):
+        for j in range(0,len(list[i])):
+            list[i][j]=(list[i][j][0]/1,list[i][j][1]/15,list[i][j][2]/127,list[i][j][3]/127,list[i][j][4]/1000)
+    return list
+
+def normalizeY(list):
+    for i in range(0,len(list)):
+        list[i]=(list[i][0]/1,list[i][1]/15,list[i][2]/127,list[i][3]/127,list[i][4]/1000)
+    return list
+    
+x_train=normalize(x_train)
+y_train=normalizeY(y_train)
 
 model=keras.Sequential()
 model.add(keras.Input(shape=(500,5)))
@@ -30,7 +44,7 @@ model.add(keras.layers.GRU(5,activation='linear'))
 model.add(keras.layers.Dense(1*5))
 
 model.compile(
-        loss=keras.losses.MeanAbsoluteError(),
+        loss=keras.losses.MeanSquaredError(),
         optimizer=keras.optimizers.Adam(lr=0.001),
         metrics=["accuracy"]
     )
@@ -39,21 +53,23 @@ model.fit(x_train,y_train)
 
 # song generation
 
-mid = MidiFile()
-track = MidiTrack()
-
-currentNote=x_train[0].tolist()
-
-i=0
-while i<499:
-    feed=[currentNote]
-    feed=np.array(feed)
-    output=model.predict(feed)
-    output=np.absolute(output)
-    output=output[0].astype("int").tolist()
-    print(output)
-    currentNote[i+1]=output
-    i+=1 
+# =============================================================================
+# mid = MidiFile()
+# track = MidiTrack()
+# 
+# currentNote=x_train[0].tolist()
+# #print(currentNote)
+# i=0
+# while i<499:
+#     feed=[currentNote]
+#     feed=np.array(feed)
+#     output=model.predict(feed)
+#     output=np.absolute(output)
+#     output=output[0].astype("int").tolist()
+#     print(output)
+#     currentNote[i+1]=output
+#     i+=1 
+# =============================================================================
 
 # =============================================================================
 # for note in currentNote:
